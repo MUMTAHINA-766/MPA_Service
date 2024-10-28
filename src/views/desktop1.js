ed280cf9328d970c495cdbb23339173e56ef9936
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2024-10-28 16:27:47 +0600

    Add files via upload

---
+++ src/views/desktop1.js
@@ -0,0 +1,408 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './desktop1.css'
+
+const Desktop1 = (props) => {
+  return (
+    <div className="desktop1-container1">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="desktop1-desktop1">
+        <div className="desktop1-container2">
+          <div className="desktop1-frame2">
+            <span className="desktop1-text100">
+              <span>
+                If you are looking for truly dignifying services of web design
+                &amp; development then in all of India Nibble Software
+                Technologies is the right place to be. A web page is a
+                fundamental element on the internet and needs to be attractive,
+                usefully informative, and mobile friendly. We understand the
+                needs of our clients and make their websites ready for the
+                online market.
+              </span>
+              <br></br>
+              <span>
+                Performing the definition and the execution of the internet
+                strategy we conduct an in-depth analysis of what your websites
+                will require in the current market. Following are the kinds of
+                websites we have made before and provide the services like CMS
+                websites, Customized software solutions, E-Commerce websites,
+                Mobile Apps, Travel Portal and API integrations.
+              </span>
+            </span>
+            <div className="desktop1-itemreactpng"></div>
+            <div className="desktop1-itemvuepng"></div>
+            <div className="desktop1-itemsqlpng"></div>
+            <div className="desktop1-itemcodepng"></div>
+            <div className="desktop1-itemphppng"></div>
+            <div className="desktop1-itemlaravelpng"></div>
+            <div className="desktop1-itemshophypng"></div>
+            <div className="desktop1-itemnodepng"></div>
+            <span className="desktop1-text104">
+              <span className="desktop1-text105">Web Service</span>
+              <br></br>
+              <span className="desktop1-text107"></span>
+              <span>Design &amp; Markup</span>
+            </span>
+            <div className="desktop1-itemmagentopng"></div>
+            <div className="desktop1-itemwordpresspng"></div>
+          </div>
+        </div>
+        <div className="desktop1-group6">
+          <div className="desktop1-group5">
+            <div className="desktop1-group4">
+              <div className="desktop1-group31">
+                <img
+                  alt="IMAGE3drenderingcomputerdesk1830"
+                  src="/external/image3drenderingcomputerdesk1830-vlt-1400w.png"
+                  className="desktop1image3drenderingcomputerdesk1"
+                />
+                <span className="desktop1-text109">
+                  <span>About</span>
+                </span>
+                <span className="desktop1-text111">
+                  <span>Blog</span>
+                </span>
+                <span className="desktop1-text113">
+                  <span>Contact</span>
+                </span>
+                <span className="desktop1-text115">
+                  <span>Services</span>
+                </span>
+              </div>
+              <div className="desktop1-group2"></div>
+            </div>
+            <span className="desktop1-text117">
+              <span>We integrate your business</span>
+            </span>
+            <span className="desktop1-text119">
+              <span className="desktop1-text120">
+                A Leader in Integrated Business
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="desktop1-text121">Software Solutions</span>
+              <span> in Bangladesh...</span>
+            </span>
+          </div>
+          <span className="desktop1-text123">
+            <span className="desktop1-text124">
+              M P
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="desktop1-text125">A</span>
+            <br className="desktop1-text126"></br>
+          </span>
+        </div>
+        <img
+          alt="Fastforward1742"
+          src="/external/fastforward1742-qys.svg"
+          className="desktop1-fastforward1"
+        />
+        <img
+          alt="Fastforward2310"
+          src="/external/fastforward2310-aqk6.svg"
+          className="desktop1-fastforward2"
+        />
+        <img
+          alt="Fastforward2614"
+          src="/external/fastforward2614-8d8.svg"
+          className="desktop1-fastforward3"
+        />
+        <div className="desktop1-group9">
+          <button className="desktop1-button1">
+            <span className="desktop1-text127 Heading">Web Development</span>
+          </button>
+          <span className="desktop1-text128 Heading">E-commerce</span>
+          <span className="desktop1-text129 Heading">App Development</span>
+          <span className="desktop1-text130 Heading">
+            <span>Digital Marketing</span>
+          </span>
+          <span className="desktop1-text132 Heading">
+            <span>API Integration</span>
+            <br></br>
+            <span></span>
+          </span>
+          <span className="desktop1-text136 Heading">
+            <span>Software Development</span>
+          </span>
+        </div>
+        <span className="desktop1-text138">
+          <span>Our Services</span>
+        </span>
+        <div className="desktop1-group81">
+          <div className="desktop1-group71">
+            <div className="desktop1-stats-card">
+              <img
+                alt="ClockI154"
+                src="/external/clocki154-scvm.svg"
+                className="desktop1-clock"
+              />
+              <div className="desktop1-review-body">
+                <div className="desktop1-text-heading">
+                  <span className="desktop1-text140 Heading"></span>
+                </div>
+                <div className="desktop1-text141">
+                  <span className="desktop1-text142"></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="desktop1-group16">
+          <div className="desktop1-container3">
+            <div className="desktop1-container4"></div>
+            <span className="desktop1-text143">
+              <span>Partners Integrations </span>
+              <span>available</span>
+            </span>
+          </div>
+          <span className="desktop1-text146">
+            <span>View All Our Integration Partners</span>
+          </span>
+        </div>
+        <div className="desktop1-container5">
+          <img
+            alt="Rectangle42275"
+            src="/external/rectangle42275-r58o-1400w.png"
+            className="desktop1-rectangle4"
+          />
+          <img
+            alt="Rectangle52276"
+            src="/external/rectangle52276-peto-1300w.png"
+            className="desktop1-rectangle5"
+          />
+        </div>
+        <img
+          alt="Rectangle82612"
+          src="/external/rectangle82612-bxcc-1400w.png"
+          className="desktop1-rectangle8"
+        />
+        <span className="desktop1-text148">Blog</span>
+        <div className="desktop1-container6">
+          <div className="desktop1-heading3">
+            <span className="desktop1-text149">
+              <span>Contact info</span>
+            </span>
+          </div>
+          <span className="desktop1-text151">
+            <span>Call Us for Query</span>
+          </span>
+          <div className="desktop1-link1">
+            <span className="desktop1-text153">
+              <span> +8800-2030-13</span>
+            </span>
+            <img
+              alt="Vector2698"
+              src="/external/vector2698-3s9.svg"
+              className="desktop1-vector1"
+            />
+          </div>
+          <div className="desktop1-link2">
+            <span className="desktop1-text155">
+              <span> +8801 997-106-6697</span>
+            </span>
+            <img
+              alt="Vector2610"
+              src="/external/vector2610-0qky.svg"
+              className="desktop1-vector2"
+            />
+          </div>
+          <span className="desktop1-text157">
+            <span>Mail Us For Quotation</span>
+          </span>
+          <div className="desktop1-link3">
+            <img
+              alt="Icon2610"
+              src="/external/icon2610-b5hg.svg"
+              className="desktop1-icon"
+            />
+            <span className="desktop1-text159">
+              <span> info@mpasoftware.com</span>
+            </span>
+          </div>
+        </div>
+        <div className="desktop1-container7">
+          <img
+            alt="Rectangle92612"
+            src="/external/rectangle92612-4i9j-200h.png"
+            className="desktop1-rectangle9"
+          />
+          <span className="desktop1-text161">
+            <span>Copyright © 2024 mpa Software Technologies Pvt Ltd.</span>
+          </span>
+        </div>
+        <div className="desktop1-heading2">
+          <span className="desktop1-text163">
+            <span>Our Location</span>
+          </span>
+        </div>
+        <span className="desktop1-text165">
+          <span>6201</span>
+          <br></br>
+          <span>
+            Court Station,
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span>
+            Razpara Thana,
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span>
+            Rajshahi,
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span>Bangladesh</span>
+        </span>
+        <span className="desktop1-text175">
+          <span>Quick Links</span>
+        </span>
+        <div className="desktop1-list">
+          <div className="desktop1-item-link1">
+            <span className="desktop1-text177">
+              <span>Home</span>
+            </span>
+          </div>
+        </div>
+        <div className="desktop1-item-link2">
+          <span className="desktop1-text179">
+            <span>
+              Contact
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+        </div>
+        <div className="desktop1-item-link3">
+          <span className="desktop1-text181">Blog</span>
+        </div>
+        <div className="desktop1-item-link4">
+          <span className="desktop1-text182">
+            <span>About</span>
+          </span>
+        </div>
+        <button className="desktop1-button2">
+          <span className="desktop1-text184 M3labelsmall">Reach Us</span>
+        </button>
+        <div className="desktop1-frame1"></div>
+        <span className="desktop1-text185">
+          <span className="desktop1-text186">What You Get At</span>
+          <br></br>
+          <span className="desktop1-text188"></span>
+          <span className="desktop1-text189">MP</span>
+          <span className="desktop1-text190">A</span>
+          <br></br>
+          <span className="desktop1-text192"></span>
+          <span>Software Technologies</span>
+        </span>
+        <div className="desktop1-group82">
+          <div className="desktop1-group72">
+            <img
+              alt="Cpu1739"
+              src="/external/cpu1739-cuo7.svg"
+              className="desktop1-cpu"
+            />
+          </div>
+        </div>
+        <div className="desktop1-group32">
+          <div className="desktop1-group1">
+            <img
+              alt="checkindeterminatesmall838"
+              src="/external/checkindeterminatesmall838-htxb.svg"
+              className="desktop1-checkindeterminatesmall1"
+            />
+            <img
+              alt="checkindeterminatesmall847"
+              src="/external/checkindeterminatesmall847-1y1p.svg"
+              className="desktop1-checkindeterminatesmall2"
+            />
+            <img
+              alt="checkindeterminatesmall844"
+              src="/external/checkindeterminatesmall844-4mcum.svg"
+              className="desktop1-checkindeterminatesmall3"
+            />
+          </div>
+        </div>
+        <span className="desktop1-text194">
+          <span>
+            Recognized as a symbol of trust and excellence, MPA Software
+            Technologies is a reward to its clients. We are a one-stop solution
+            for all IT needs like website, CRM, travel portal, travel APIs,
+            mobile apps and digital marketing needs. With a team of
+            professionals, we further aim to strive for exceptional results in
+            terms of work and ethics. We believe that our smart marketing
+            strategies will fetch higher exposure, more clients and generate
+            business, help you achieve your goals. We professional team for
+            every need of our clients.
+          </span>
+          <br></br>
+          <span>
+            Improvements through difficult challenges are something we always
+            look forward to and we never shy away from opportunities.
+            Transforming your ideas into web has been our biggest achievement so
+            far.
+          </span>
+        </span>
+        <img
+          alt="IMAGE5b99491e08e44a83be19192bb00c8a7b12626"
+          src="/external/image5b99491e08e44a83be19192bb00c8a7b12626-xv3h-500w.png"
+          className="desktop1image5b99491e08e44a83be19192bb00c8a7b1"
+        />
+        <span className="desktop1-text198">
+          <span>All in one Business Solution POS Software</span>
+        </span>
+        <div className="desktop1-container8">
+          <img
+            alt="Rectangle72312"
+            src="/external/rectangle72312-4oi.svg"
+            className="desktop1-rectangle7"
+          />
+          <div className="desktop1-group17"></div>
+        </div>
+        <img
+          alt="rb153512848"
+          src="/external/rb153512848-odr7-400h.png"
+          className="desktop1-rb15351"
+        />
+        <div className="desktop1-section">
+          <div className="desktop1-itempusherpng"></div>
+          <div className="desktop1-itemsendgridpng"></div>
+          <div className="desktop1-itemtbopng"></div>
+        </div>
+        <div className="desktop1-iteminstamojopng"></div>
+        <div className="desktop1-itemawspng"></div>
+        <div className="desktop1-itemsabrepng"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Desktop1
